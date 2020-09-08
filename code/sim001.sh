7ee8d762d3a03b3b043de709b25590f6e25a1884
phuchng <hongphucng63@gmail.com>
2020-09-07 21:54:16 -0400

    Fix typo in slurm

--- code/sim001.sh
+++ code/sim001.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -o slurm.out
 #SBATCH -e slurm.err 
-#SBATCH -–mem=32G
-#SBATCH –p herringlab
+#SBATCH --mem=32G
+#SBATCH -p herringlab
 module load R/x86_64-pc-linux-gnu-library/3.6
 R CMD BATCH sim001.R
