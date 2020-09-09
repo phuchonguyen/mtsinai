84630fd9bc36f72df16224aa446a216a939e23f6
phuchng <hongphucng63@gmail.com>
2020-09-09 08:43:06 -0400

    Add R source

--- code/sim001.sh
+++ code/sim001.sh
@@ -3,5 +3,6 @@
 #SBATCH -e slurm.err 
 #SBATCH --mem=8G
 #SBATCH -p herringlab,statdept-low,volfovskylab-low
+export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/3.6
 module load R/3.6
 R CMD BATCH sim001.R
