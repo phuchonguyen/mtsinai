1eee896e0356c00779cde1ab6325a12e7c074e12
phuchng <hongphucng63@gmail.com>
2020-09-09 08:50:30 -0400

    Remove xtra spaces

--- code/sim001.sh
+++ code/sim001.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH -o slurm.out
 #SBATCH -e slurm.err 
+#SBATCH -account=herringlab
 #SBATCH --mem=8G
 #SBATCH -p herringlab,statdept-low,volfovskylab-low
-export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/3.6
+export R_LIBS_USER=~/R/x86_64-pc-linux-gnu-library/3.6
 module load R/3.6
 R CMD BATCH sim001.R
