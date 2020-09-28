49f1f5b2b309b517e1521765dd05eca3111d433f
phuchng <hongphucng63@gmail.com>
2020-09-28 17:51:00 -0400

    Change file path to hpc

--- code/simSing.sh
+++ code/simSing.sh
@@ -5,5 +5,5 @@
 #SBATCH -c32
 #SBATCH -p herringlab,statdept-low,volfovskylab-low
 
-singularity exec --bind /work/phn5/mtsinai/mtsinai/code/samples mtsinai.sif ls samples
+singularity exec --bind /hpc/home/phn5/mtsinai/code/samples mtsinai.sif ls samples
 singularity exec mtsinai.sif Rscript simSing.R
