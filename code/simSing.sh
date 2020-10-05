bcea9399286f8fdcf7da455fe31b2f0b970d3728
phuchng <hongphucng63@gmail.com>
2020-10-05 01:22:54 -0400

    Fix scalar dot prod err

--- code/simSing.sh
+++ code/simSing.sh
@@ -4,6 +4,11 @@
 #SBATCH --account=herringlab
 #SBATCH -c32
 #SBATCH -p herringlab,statdept-low,volfovskylab-low
+#SBATCH --mail-user=phn5@duke.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
 
 singularity exec --bind /hpc/home/phn5/mtsinai/code/samples mtsinai.sif ls samples
 singularity run mtsinai.sif simSing.R
